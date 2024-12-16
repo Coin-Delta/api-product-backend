a9fb13659586c8a3c5b982d479700134b52c05a7
chirag mehta <chirag@web4next.com>
2024-12-16 12:56:25 +0530

    created dynamic controller

---
+++ app/controllers/apiProduct/dynamicValidationController.js
@@ -0,0 +1,88 @@
+const ResponseHelper = require('../../utils/responseHelper')
+const APIService = require('../../services/apiService.js')
+const BaseError = require('../../utils/error/baseError.js')
+// const MOCK_RESPONSES = require('../../utils/mockData')
+const NEW_MOCK_RESPONSES = require('../../utils/newMockData.js')
+
+// const { STATUS_CODES } = require('../../constants/statusCodes.js')
+
+class DynamicController {
+  static async verifyDocument(req, res) {
+    let documentType
+    try {
+      const { apiId, documentData } = req.body
+      const { bcaId: clientId } = req.user
+
+      const apiDetails = await APIService.getAPIDetails(apiId)
+      console.log('api details:', apiDetails)
+      documentType = apiDetails.documentType
+
+      const { statusCode, apiResponse } =
+        await APIService.processDocumentAndUpdateBalance({
+          // apiId,
+          apiDetails,
+          documentData,
+          clientId
+        })
+
+      // console.log('req:', req)
+      console.log('apiResponse controller:', apiResponse)
+
+      return ResponseHelper.success(
+        res,
+        apiResponse,
+        `${documentType} Verification successfull`,
+        statusCode
+      )
+    } catch (error) {
+      if (error instanceof BaseError) {
+        console.log('error msg:', error.message)
+        console.log('full err:', error)
+        if (documentType) {
+          return ResponseHelper.error(
+            res,
+            `${documentType} Verification failed`,
+            error.statusCode,
+            error
+          )
+        }
+        return ResponseHelper.error(res, error.message, error.statusCode, error)
+      }
+      return ResponseHelper.serverError(res, error)
+    }
+  }
+
+  static async verifyDocumentTest(req, res) {
+    let documentType
+    try {
+      const { apiId, documentData } = req.body
+      const apiDetails = await APIService.getAPIDetails(apiId)
+      console.log('api details:', apiDetails)
+      documentType = apiDetails.documentType
+
+      // Return success or failure mock response based on whether documentData is provided
+      const mockResponse = documentData
+        ? NEW_MOCK_RESPONSES[documentType]?.success.data
+        : NEW_MOCK_RESPONSES[documentType]?.failure.data
+
+      return mockResponse.success
+        ? ResponseHelper.success(
+            res,
+            mockResponse.data,
+            mockResponse.message,
+            mockResponse.status_code
+          )
+        : ResponseHelper.error(
+            res,
+            mockResponse.message,
+            mockResponse.status_code,
+            mockResponse.data
+          )
+    } catch (error) {
+      console.log(error)
+      return ResponseHelper.serverError(res, error)
+    }
+  }
+}
+
+module.exports = DynamicController
--- app/routes/apiProduct.js
+++ app/routes/apiProduct.js
@@ -15,6 +15,7 @@ const CorporateCINController = require('../controllers/apiProduct/corporateCINCo
 const CorporateGSTINController = require('../controllers/apiProduct/corporateGSTINController')
 const CreditReportController = require('../controllers/apiProduct/creditReportController')
 const CreditReportPDFController = require('../controllers/apiProduct/creditReportPdfController')
+const DynamicController = require('../controllers/apiProduct/dynamicValidationController.js')
 
 /*
  * API Product route
@@ -89,6 +90,7 @@ router.post(
     : BankVerificationPennylessController.verifyBankDetails
 )
 
+
 router.post(
   '/coporate-cin',
   trimRequest.all,
@@ -121,6 +123,15 @@ router.post(
   process.env.TEST_MODE === 'true'
     ? CreditReportPDFController.verifyCreditReportPdfTest
     : CreditReportPDFController.verifyCreditReportPdf
+)
+
+router.post(
+  '/dynamic',
+  trimRequest.all,
+  verifyJWT,
+  process.env.TEST_MODE === 'true'
+    ? DynamicController.verifyDocumentTest
+    : DynamicController.verifyDocument
 )
 
 module.exports = router
