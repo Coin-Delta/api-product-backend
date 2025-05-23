67f8bf513c0a80b30e5abfce7184a4a995a09578
kartik-coindelta <kartik@web4next.com>
2025-05-23 18:14:40 +0530

    created the api for excel report for apiTransaction logs

--- app/routes/apiTransactionLogs.js
+++ app/routes/apiTransactionLogs.js
@@ -3,12 +3,21 @@ const router = express.Router()
 const trimRequest = require('trim-request')
 
 const { verifyJWT } = require('../middleware/auth')
-const { getClientTransactions } = require('../controllers/apiTransactionLogs')
+const {
+  getClientTransactions,
+  getClientTransactionInExcel
+} = require('../controllers/apiTransactionLogs')
 
 /*
  * API Product route
  */
 
 router.get('/logs', trimRequest.all, verifyJWT, getClientTransactions)
+router.get(
+  '/logs/export',
+  trimRequest.all,
+  verifyJWT,
+  getClientTransactionInExcel
+)
 
 module.exports = router
